76ed39c500f744d3c0813e8351729a73f8d49f15
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-06-19 22:44:36 +0000

    feat: add summary page to component
    Implemented the summary page as the last page of the component, providing an overview of all submitted data.

--- src/components/TenantForm/TenantForm.tsx
+++ src/components/TenantForm/TenantForm.tsx
@@ -175,6 +175,56 @@ const TenantForm = () => {
 					</div>
 				)}
 			</div>
+
+			{page === 4 && (
+				<div>
+					<div className='px-4 sm:px-0'>
+						<h3 className='text-base font-semibold leading-7 text-gray-900'>
+							Mieter/in Informationen
+						</h3>
+						<p className='mt-1 max-w-2xl text-sm leading-6 text-gray-500'>
+							Persönliche Daten
+						</p>
+					</div>
+					<div className='mt-6 border-t border-gray-100'>
+						<dl className='divide-y divide-gray-100'>
+							<div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'>
+								<dt className='text-sm font-medium leading-6 text-gray-900'>
+									Vollständiger Name
+								</dt>
+								<dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+									{fullName}
+								</dd>
+							</div>
+							<div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'>
+								<dt className='text-sm font-medium leading-6 text-gray-900'>
+									E-Mail Adresse
+								</dt>
+								<dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+									{email}
+								</dd>
+							</div>
+							<div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'>
+								<dt className='text-sm font-medium leading-6 text-gray-900'>
+									Telefonnummer
+								</dt>
+								<dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+									{phoneNumber}
+								</dd>
+							</div>
+							<div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'>
+								<dt className='text-sm font-medium leading-6 text-gray-900'>
+									Gehaltsangabe
+								</dt>
+								<dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+									{salary.replace(/-/g, ' - ').replace(/>/g, 'Mehr als ')}€
+								</dd>
+							</div>
+						</dl>
+					</div>
+				</div>
+			)}
+
 			<div className='mt-6 flex items-center justify-end gap-x-6'>
 				{/* Is not rendering when on page 0, to prevent falsie state value */}
 				{page !== 0 && (
@@ -189,7 +239,7 @@ const TenantForm = () => {
 					onClick={nextPage}
 					type='button'
 					className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-					Weiter
+					{page === 4 ? 'Absenden' : page === 3 ? 'Zusammenfassung' : 'Weiter'}
 				</button>
 			</div>
 		</form>
