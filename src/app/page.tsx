e60926b145bc61a012b722487b33a45e5be3355b
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-06-20 19:27:39 +0000

    style: add responsive padding to main element
    Added different padding sizes to the main element for various breakpoints.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import TenantForm from '@/components/TenantForm';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col justify-between p-12'>
+		<main className='flex min-h-screen flex-col justify-between p-6 md:p-8 lg:p-10 xl:p-12 '>
 			<TenantForm />
 		</main>
 	);
