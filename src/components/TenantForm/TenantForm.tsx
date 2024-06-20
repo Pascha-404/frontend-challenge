85b94d8cd36173e392cc068987c04a81936c6b62
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-06-20 20:47:17 +0000

    style: add dark mode styling for summary page
    - Added dark mode styling for the summary page.
    - Adjusted text colors for better contrast.
    - Applied italic style to user-provided data for emphasis.
    - Increased the top margin of sections for improved spacing.
    - These changes enhance readability and provide a better user experience.

--- src/components/TenantForm/TenantForm.tsx
+++ src/components/TenantForm/TenantForm.tsx
@@ -139,12 +139,12 @@ const TenantForm = () => {
 
 			{/* Code block for summary page */}
 			{page === 4 && (
-				<div>
+				<div className='mt-12'>
 					<div className='px-4 sm:px-0'>
-						<h2 className='text-base font-semibold leading-7 text-gray-900'>
+						<h2 className='text-2xl md:text-3xl font-semibold dark:text-white'>
 							Mieter/in Informationen
 						</h2>
-						<p className='mt-1 max-w-2xl text-sm leading-6 text-gray-500'>
+						<p className='mt-4 text-base md:text-lg leading-6 text-gray-600 dark:text-gray-400'>
 							Persönliche Daten
 						</p>
 					</div>
@@ -154,10 +154,10 @@ const TenantForm = () => {
 								<div
 									key={`summaryItem-${idx}`}
 									className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'>
-									<dt className='text-sm font-medium leading-6 text-gray-900'>
+									<dt className='text-sm font-medium leading-6 text-gray-900 dark:text-neutral-300'>
 										{item.dataName}
 									</dt>
-									<dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
+									<dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 dark:text-gray-100 italic'>
 										{item.dataValue}
 									</dd>
 								</div>
