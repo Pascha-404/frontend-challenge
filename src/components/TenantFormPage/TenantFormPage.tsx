de006b69ac561f3305044bee0d3c20256dbc7e9c
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-06-20 10:35:08 +0000

    feat: extract JSX from TenantForm into component
    Extracted pages JSX from the TenantForm component that do not contain a fieldset. Renamed props and passed them to the associated JSX elements. Created types for autoComplete and inputType props.

--- src/components/TenantFormPage/TenantFormPage.tsx
+++ src/components/TenantFormPage/TenantFormPage.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
+type TInputType = 'email' | 'text' | 'tel' | 'radio';
+type TAutoComplete = 'name' | 'email' | 'tel';
+
 interface ITenantFormPage {
 	header: string;
 	subheader?: string;
 	labelText: string;
-	id: string;
+	inputId: string;
 	placeholder?: string;
-	type: string;
+	inputType: TInputType;
 	fieldset?: boolean;
 	fieldsetData?: string[];
+	autoCompleteType?: TAutoComplete;
 	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
@@ -16,14 +20,49 @@ const TenantFormPage: React.FC<ITenantFormPage> = ({
 	header,
 	subheader,
 	labelText,
-	id,
+	inputId, // is beeing utilised as htmlFor, name, id, value
 	placeholder,
-	type,
+	inputType,
+	autoCompleteType,
 	fieldset = false,
 	fieldsetData = [],
 	onChange,
 }) => {
-	return <div></div>;
+	return (
+		<div className='pb-12'>
+			<h2 className='text-2xl md:text-3xl font-semibold dark:text-white'>{header}</h2>
+
+			{subheader && (
+				<p className='mt-4 text-base md:text-lg leading-6 text-gray-600'>{subheader}</p>
+            )}
+            
+			<div className='mt-16 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+				<div className='sm:col-span-4'>
+					<label
+						htmlFor={inputId}
+						className='block text-sm font-normal leading-tight text-stone-600 dark:text-neutral-300'>
+						{labelText}
+					</label>
+
+					<div className='mt-2'>
+						<div className='flex rounded-xl shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md'>
+							<input
+								type={inputType}
+								name={inputId}
+								id={inputId}
+								required
+								onChange={onChange}
+								value={inputId}
+								autoComplete={autoCompleteType}
+								className='block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-2 sm:text-sm sm:leading-6 focus:outline-none focus-within:ring-2 focus-within:ring-inset focus-within:ring-black rounded-xl'
+								placeholder={placeholder}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default TenantFormPage;
