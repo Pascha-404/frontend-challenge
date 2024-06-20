ffdbdf740d505a73ee81fae4fed37e0bc16bb1d4
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-06-20 12:41:28 +0000

    style: remove align-center class
    Removed align-center class for a consistent layout on the left side of the screen.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import TenantForm from '@/components/TenantForm';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-12'>
+		<main className='flex min-h-screen flex-col justify-between p-12'>
 			<TenantForm />
 		</main>
 	);
