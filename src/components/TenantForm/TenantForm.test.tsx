ade551b9ccfef1131b5ccb727e5cbf385fd1a970
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2024-06-21 14:19:09 +0000

    test: modify file to use waitFor
    - Using framer-motion for animating mounting/unmounting form content caused errors in the test suite.
    - Changed all test cases to async functions.
    - Introduced the testing-library waitFor method to wait for animated content.

--- src/components/TenantForm/TenantForm.test.tsx
+++ src/components/TenantForm/TenantForm.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import TenantForm from './TenantForm';
 import { IFormData, useFormStore } from '@/store/useFormStore';
 
@@ -44,63 +44,81 @@ const renderComponent = (
 
 describe('TenantForm Component', () => {
 	// Test if the component renders correctly with the initial page
-	it('renders the form correctly', () => {
+	it('renders the form correctly', async () => {
 		renderComponent();
-		expect(screen.getByRole('form')).toBeInTheDocument();
-		expect(screen.getByTestId('progressbar')).toBeInTheDocument();
-		expect(screen.getByText('Wie lautet Ihr vollständiger Name?')).toBeInTheDocument();
-		expect(screen.getByLabelText('Name eingeben')).toBeInTheDocument();
-		expect(screen.getByPlaceholderText('z.B. Maxima Mustermann'));
-		expect(screen.getByText('Weiter')).toBeDisabled();
-		expect(screen.queryByText('Zurück')).not.toBeInTheDocument();
+		await waitFor(() => expect(screen.getByRole('form')).toBeInTheDocument());
+		await waitFor(() => expect(screen.getByTestId('progressbar')).toBeInTheDocument());
+		await waitFor(() =>
+			expect(screen.getByText('Wie lautet Ihr vollständiger Name?')).toBeInTheDocument()
+		);
+		await waitFor(() =>
+			expect(screen.getByLabelText('Name eingeben')).toBeInTheDocument()
+		);
+		await waitFor(() => expect(screen.getByPlaceholderText('z.B. Maxima Mustermann')));
+		await waitFor(() => expect(screen.getByText('Weiter')).toBeDisabled());
+		await waitFor(() => expect(screen.queryByText('Zurück')).not.toBeInTheDocument());
 	});
 
 	// Test the handleChange function
-	it('calls handleChange and updates state correctly', () => {
+	it('calls handleChange and updates state correctly', async () => {
 		renderComponent();
 		const nameInput = screen.getByLabelText('Name eingeben');
 		fireEvent.change(nameInput, { target: { value: 'Maxima Mustermann' } });
-		expect(nameInput).toHaveValue('Maxima Mustermann');
+		await waitFor(() => expect(nameInput).toHaveValue('Maxima Mustermann'));
 	});
 
 	// Test if the component switches to the next page on 'Weiter' button click
-	it('navigates to the next page when "Weiter" button is clicked', () => {
+	it('navigates to the next page when "Weiter" button is clicked', async () => {
 		renderComponent({ fullName: 'Maxima Mustermann' });
 		const nextButton = screen.getByText('Weiter');
 		fireEvent.click(nextButton);
-		expect(screen.getByLabelText('E-Mail eingeben')).toBeInTheDocument();
-		expect(screen.getByText('Zurück')).toBeInTheDocument();
-		expect(screen.getByText('Weiter')).toBeDisabled();
+		await waitFor(() =>
+			expect(screen.getByLabelText('E-Mail eingeben')).toBeInTheDocument()
+		);
+		await waitFor(() => expect(screen.getByText('Zurück')).toBeInTheDocument());
+		await waitFor(() => expect(screen.getByText('Weiter')).toBeDisabled());
 	});
 
 	// Test if the component switches to the previous page on 'Zurück' button click
-	it('navigates to the previous page when "Zurück" button is clicked', () => {
+	it('navigates to the previous page when "Zurück" button is clicked', async () => {
 		renderComponent({ fullName: 'Maxima Mustermann' }, 1);
-		expect(screen.getByLabelText('E-Mail eingeben')).toBeInTheDocument();
+		await waitFor(() =>
+			expect(screen.getByLabelText('E-Mail eingeben')).toBeInTheDocument()
+		);
 		const prevButton = screen.getByText('Zurück');
 		fireEvent.click(prevButton);
-		expect(screen.getByText('Wie lautet Ihr vollständiger Name?')).toBeInTheDocument();
+		await waitFor(() =>
+			expect(screen.getByText('Wie lautet Ihr vollständiger Name?')).toBeInTheDocument()
+		);
 	});
 
 	// Test if the phoneNumber page renders correctly
-	it('renders the form on the third page with phone number', () => {
+	it('renders the form on the third page with phone number', async () => {
 		renderComponent({ phoneNumber: '123456789' }, 2);
-		expect(screen.getByText('Wie lautet Ihre Telefonnummer?')).toBeInTheDocument();
-		expect(screen.getByLabelText('Telefonnummer eingeben')).toHaveValue('123456789');
-		expect(screen.getByText('Zurück')).toBeInTheDocument();
+		await waitFor(() =>
+			expect(screen.getByText('Wie lautet Ihre Telefonnummer?')).toBeInTheDocument()
+		);
+		await waitFor(() =>
+			expect(screen.getByLabelText('Telefonnummer eingeben')).toHaveValue('123456789')
+		);
+		await waitFor(() => expect(screen.getByText('Zurück')).toBeInTheDocument());
 	});
 
 	// Test if the salary page renders correctly
-	it('renders the form on the fourth page with salary options', () => {
+	it('renders the form on the fourth page with salary options', async () => {
 		renderComponent({}, 3);
-		expect(screen.getByText('Angaben zum Einkommen')).toBeInTheDocument();
-		expect(screen.getByText('Einkommen auswählen')).toBeInTheDocument();
-		expect(screen.getByText('Zurück')).toBeInTheDocument();
-		expect(screen.getByText('Zusammenfassung')).toBeInTheDocument();
+		await waitFor(() =>
+			expect(screen.getByText('Angaben zum Einkommen')).toBeInTheDocument()
+		);
+		await waitFor(() =>
+			expect(screen.getByText('Einkommen auswählen')).toBeInTheDocument()
+		);
+		await waitFor(() => expect(screen.getByText('Zurück')).toBeInTheDocument());
+		await waitFor(() => expect(screen.getByText('Zusammenfassung')).toBeInTheDocument());
 	});
 
 	// Test if the summary page renders correctly
-	it('renders the summary page correctly', () => {
+	it('renders the summary page correctly', async () => {
 		renderComponent(
 			{
 				fullName: 'Maxima Mustermann',
@@ -110,15 +128,23 @@ describe('TenantForm Component', () => {
 			},
 			4
 		);
-		expect(screen.getByText('Mieter/in Informationen')).toBeInTheDocument();
-		expect(screen.getByText('Vollständiger Name')).toBeInTheDocument();
-		expect(screen.getByText('Maxima Mustermann')).toBeInTheDocument();
-		expect(screen.getByText('E-Mail Adresse')).toBeInTheDocument();
-		expect(screen.getByText('M.Mustermann@example.com')).toBeInTheDocument();
-		expect(screen.getByText('Telefonnummer')).toBeInTheDocument();
-		expect(screen.getByText('017712345')).toBeInTheDocument();
-		expect(screen.getByText('Gehaltsangabe')).toBeInTheDocument();
-		expect(screen.getByText('Mehr als 4000€')).toBeInTheDocument();
-		expect(screen.getByText('Absenden')).toBeInTheDocument();
+		await waitFor(() =>
+			expect(screen.getByText('Mieter/in Informationen')).toBeInTheDocument()
+		);
+		await waitFor(() =>
+			expect(screen.getByText('Vollständiger Name')).toBeInTheDocument()
+		);
+		await waitFor(() =>
+			expect(screen.getByText('Maxima Mustermann')).toBeInTheDocument()
+		);
+		await waitFor(() => expect(screen.getByText('E-Mail Adresse')).toBeInTheDocument());
+		await waitFor(() =>
+			expect(screen.getByText('M.Mustermann@example.com')).toBeInTheDocument()
+		);
+		await waitFor(() => expect(screen.getByText('Telefonnummer')).toBeInTheDocument());
+		await waitFor(() => expect(screen.getByText('017712345')).toBeInTheDocument());
+		await waitFor(() => expect(screen.getByText('Gehaltsangabe')).toBeInTheDocument());
+		await waitFor(() => expect(screen.getByText('Mehr als 4000€')).toBeInTheDocument());
+		await waitFor(() => expect(screen.getByText('Absenden')).toBeInTheDocument());
 	});
 });
